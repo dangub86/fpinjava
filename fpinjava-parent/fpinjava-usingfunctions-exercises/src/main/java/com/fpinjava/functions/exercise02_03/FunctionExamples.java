c800d9d009d38745198120ee93a8054e1bc12855
Daniele Gubbiotti <gubbiottidaniele@gmail.com>
2024-08-27 18:21:19 +0200

    2.3

--- fpinjava-parent/fpinjava-usingfunctions-exercises/src/main/java/com/fpinjava/functions/exercise02_03/FunctionExamples.java
+++ fpinjava-parent/fpinjava-usingfunctions-exercises/src/main/java/com/fpinjava/functions/exercise02_03/FunctionExamples.java
@@ -3,9 +3,10 @@ package com.fpinjava.functions.exercise02_03;
 
 public class FunctionExamples {
 
-  public static final Function<Integer, Function<Integer, Integer>> add = null; // To be implemented
+  public static final Function<Integer, Function<Integer, Integer>> add =
+          arg -> (arg2 -> arg + arg2);
 
-  public static final BinaryOperator add2 =  null; // To be implemented
+  public static final BinaryOperator add2 =  arg -> (arg2 -> arg + arg2);
 
-  public static final BinaryOperator mult =  null; // To be implemented
+  public static final BinaryOperator mult =  arg -> (arg2 -> arg * arg2);
 }
