b0f65f726ec88bb70b137c9d89055d081a92ad8e
daniele.gubbiotti <gubbiottidaniele@gmail.com>
2024-09-10 09:07:43 +0200

    added overflow risk test in 2.1

--- fpinjava-parent/fpinjava-usingfunctions-exercises/src/test/java/com/fpinjava/functions/exercise02_01/FunctionExamplesTest.java
+++ fpinjava-parent/fpinjava-usingfunctions-exercises/src/test/java/com/fpinjava/functions/exercise02_01/FunctionExamplesTest.java
@@ -16,4 +16,19 @@ public class FunctionExamplesTest {
     assertEquals(Integer.valueOf(36), square.apply(triple.apply(2)));
     assertEquals(Integer.valueOf(27), compose(triple, square).apply(3));
   }
+
+  @Test
+  public void testOverflowRisks() {
+    int fnum = 10_000;
+    Function<Integer, Integer> g = x -> x;
+    Function<Integer, Integer> f = x -> x + 1;
+
+    for (int i = 0; i < fnum; i++) {
+      g = compose(f, g);
+    };
+
+    System.out.println(g.apply(0));
+  }
+
+
 }
